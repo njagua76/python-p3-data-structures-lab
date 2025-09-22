25d9ede289a5b778c3531291a0cc7b3ea6d86492
Ann Gathoni <theanngathoni@gmail.com>
2025-09-22 19:00:22 +0300

    manipulating sequences using for loop

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,37 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
+    return[food["name"] for food in spicy_foods]
     pass
 
 def get_spiciest_foods(spicy_foods):
+    return [food for food in spicy_foods if food["heat_level"]>5]
     pass
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        heat = "🌶" * food["heat_level"]
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat}")
+
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
+    for food in spicy_foods:
+      if food["cuisine"] == cuisine:
+          return food  
     pass
 
 def print_spiciest_foods(spicy_foods):
+   for food in spicy_foods:
+    if food["heat_level"] > 5:
+        heat = "🌶" * food["heat_level"]
+        print(f"{food ['name']} ({food['cuisine']}) | Heat Level: {heat}")
     pass
 
 def get_average_heat_level(spicy_foods):
+    total = sum(food["heat_level"] for food in spicy_foods)
+    return total //len(spicy_foods)
     pass
 
 def create_spicy_food(spicy_foods, spicy_food):
+    return spicy_foods + [spicy_food]
     pass
